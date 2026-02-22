99b79bf310768de552f9136c255f6c5befea2e30
Thamaruj <thamarujalthotage1@gmail.com>
2026-02-22 20:26:16 +0530

    Expo Project Reset and NativeWind Addition Complete

---
+++ mobile-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-800">
+      <Text className="text-white text-2xl font-bold">
+        Welcome to AlertZone
+      </Text>
+    </View>
+  );
+}
