981a769422a1039221853ae868bf19e5e902a5a5
Thamaruj <thamarujalthotage1@gmail.com>
2026-02-22 22:57:17 +0530

    Set up basic mobile-app structure with login page and 3-tab screen

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -1,11 +1,21 @@
-import { View, Text } from "react-native";
+import { router } from "expo-router";
+import { View, Text, Pressable } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-800">
-      <Text className="text-white text-2xl font-bold">
+      <Text className="text-white text-4xl ">
         Welcome to AlertZone
       </Text>
+
+      <Pressable 
+        className="bg-white mt-3 px-6 py-3 rounded-xl"
+        onPress={()=>router.push("/home")}>
+        <Text className="text-blue-800 font-semibold">
+          LogIn
+        </Text>
+      </Pressable>
+
     </View>
   );
 }
