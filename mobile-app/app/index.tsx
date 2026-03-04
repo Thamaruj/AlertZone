f3337853e5b1fa5e9a359212a943e217f7c3f769
Thamaruj <thamarujalthotage1@gmail.com>
2026-03-05 02:15:30 +0530

    Connected Firebase to the Mobile-App

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -4,18 +4,22 @@ import { View, Text, Pressable } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-blue-800">
-      <Text className="text-white text-4xl ">
-        Welcome to AlertZone
+      <Text className="text-white text-4xl font-bold">
+        AlertZone
+      </Text>
+      <Text className="text-blue-100 text-lg mt-2 text-center px-10">
+        Citizen-Driven Safety & Infrastructure Reporting
       </Text>
 
+      {/* This button will now take the user to the login page to start the Firebase check */}
       <Pressable 
-        className="bg-white mt-3 px-6 py-3 rounded-xl"
-        onPress={()=>router.push("/home")}>
-        <Text className="text-blue-800 font-semibold">
-          LogIn
+        className="bg-white mt-10 px-10 py-4 rounded-2xl shadow-lg"
+        onPress={() => router.push("/(auth)/login")}
+      >
+        <Text className="text-blue-800 font-bold text-lg">
+          Get Started
         </Text>
       </Pressable>
-
     </View>
   );
 }
